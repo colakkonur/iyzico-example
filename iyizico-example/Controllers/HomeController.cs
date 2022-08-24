a1ace3cbccc553a137e2114e8e820f41bb064537
colakkonur <colakkonur@gmail.com>
2022-08-24 14:54:40 +0300

    Add project files.

---
+++ iyizico-example/Controllers/HomeController.cs
@@ -0,0 +1,155 @@
+﻿using iyizico_example.Models;
+using Iyzipay;
+using Iyzipay.Model;
+using Iyzipay.Request;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace iyizico_example.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        
+        public static String odendiMi = "";
+        public static String conversation_id = "";
+        public static String token = "";
+
+        public IActionResult Index(string? token)
+        {
+            if(token != null && token != "")
+            {
+                Options options = new Options();
+                options.ApiKey = "sandbox-YENnxGsL5HuJm8UEcHT9go0cgtMf87YW";
+                options.SecretKey = "sandbox-cVx814XiOZecsRmw9u5GlK6gXgF210HK";
+                options.BaseUrl = "https://sandbox-api.iyzipay.com";
+
+                RetrieveCheckoutFormRequest request2 = new RetrieveCheckoutFormRequest();
+                request2.Locale = "tr";
+                request2.Token = token;
+
+                CheckoutForm checkoutForm = CheckoutForm.Retrieve(request2, options);
+                if (checkoutForm.PaymentStatus == "SUCCESS")
+                {
+                    TempData["Payment_Status"] = "Ödeme işlemi başarıyla gerçekleşti.";
+                }
+                else
+                {
+                    TempData["Payment_Status"] = checkoutForm.ErrorMessage;
+                }
+            }
+            
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult GetPay(string return_url)
+        {
+
+            Options options = new Options();
+            options.ApiKey = "sandbox-YENnxGsL5HuJm8UEcHT9go0cgtMf87YW";
+            options.SecretKey = "sandbox-cVx814XiOZecsRmw9u5GlK6gXgF210HK";
+            options.BaseUrl = "https://sandbox-api.iyzipay.com";
+
+            CreateCheckoutFormInitializeRequest request = new CreateCheckoutFormInitializeRequest();
+            request.Locale = Locale.TR.ToString();
+            request.ConversationId = "123456789";
+            request.Price = "1";
+            request.PaidPrice = "1.3";
+            request.Currency = Currency.TRY.ToString();
+            request.BasketId = "B67832";
+            request.PaymentGroup = PaymentGroup.PRODUCT.ToString();
+            request.CallbackUrl = "https://localhost:7036/";
+
+            List<int> enabledInstallments = new List<int>();
+            enabledInstallments.Add(2);
+            enabledInstallments.Add(3);
+            enabledInstallments.Add(6);
+            enabledInstallments.Add(9);
+            request.EnabledInstallments = enabledInstallments;
+
+            Buyer buyer = new Buyer();
+            buyer.Id = "BY789";
+            buyer.Name = "John2";
+            buyer.Surname = "Doe2";
+            buyer.GsmNumber = "+905350000000";
+            buyer.Email = "email@email.com";
+            buyer.IdentityNumber = "74300864791";
+            buyer.LastLoginDate = "2015-10-05 12:43:35";
+            buyer.RegistrationDate = "2013-04-21 15:12:09";
+            buyer.RegistrationAddress = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
+            buyer.Ip = "85.34.78.112";
+            buyer.City = "Istanbul";
+            buyer.Country = "Turkey";
+            buyer.ZipCode = "34732";
+            request.Buyer = buyer;
+
+            Address shippingAddress = new Address();
+            shippingAddress.ContactName = "Jane Doe";
+            shippingAddress.City = "Istanbul";
+            shippingAddress.Country = "Turkey";
+            shippingAddress.Description = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
+            shippingAddress.ZipCode = "34742";
+            request.ShippingAddress = shippingAddress;
+
+            Address billingAddress = new Address();
+            billingAddress.ContactName = "Jane Doe";
+            billingAddress.City = "Istanbul";
+            billingAddress.Country = "Turkey";
+            billingAddress.Description = "Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1";
+            billingAddress.ZipCode = "34742";
+            request.BillingAddress = billingAddress;
+
+            List<BasketItem> basketItems = new List<BasketItem>();
+            BasketItem firstBasketItem = new BasketItem();
+            firstBasketItem.Id = "BI101";
+            firstBasketItem.Name = "Binocular";
+            firstBasketItem.Category1 = "Collectibles";
+            firstBasketItem.Category2 = "Accessories";
+            firstBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
+            firstBasketItem.Price = "0.3";
+            basketItems.Add(firstBasketItem);
+
+            BasketItem secondBasketItem = new BasketItem();
+            secondBasketItem.Id = "BI102";
+            secondBasketItem.Name = "Game code";
+            secondBasketItem.Category1 = "Game";
+            secondBasketItem.Category2 = "Online Game Items";
+            secondBasketItem.ItemType = BasketItemType.VIRTUAL.ToString();
+            secondBasketItem.Price = "0.5";
+            basketItems.Add(secondBasketItem);
+
+            BasketItem thirdBasketItem = new BasketItem();
+            thirdBasketItem.Id = "BI103";
+            thirdBasketItem.Name = "Usb";
+            thirdBasketItem.Category1 = "Electronics";
+            thirdBasketItem.Category2 = "Usb / Cable";
+            thirdBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
+            thirdBasketItem.Price = "0.2";
+            basketItems.Add(thirdBasketItem);
+            request.BasketItems = basketItems;
+
+            CheckoutFormInitialize checkoutFormInitialize = CheckoutFormInitialize.Create(request, options);
+            conversation_id = checkoutFormInitialize.ConversationId;
+            TempData["Checkout_Form"] = checkoutFormInitialize.CheckoutFormContent;
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
